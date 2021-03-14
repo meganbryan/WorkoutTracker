9d42404329bda089c31a03fb50eaeff1f5ae9b56
Megan Bryan <meganmbryan12@gmail.com>
2021-03-14 13:07:40 -0700

    Fix html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,15 +2,15 @@ const router = require("express").Router();
 const path = require("path");
 
 router.get("/", (req, res) => {
-    res.sendFile(path.join(dirname, "html/index.html"));
+    res.sendFile(path.join(__dirname, "../public/html/index.html"));
 });
 
 router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(dirname, "html/exercise.html"));
+    res.sendFile(path.join(__dirname, "../public/html/exercise.html"));
 });
 
 router.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname, "html/stats.html"));
+    res.sendFile(path.join(__dirname, "../public/html/stats.html"));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const workouts = require("./workoutModel.js");
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbWorkout", { useNewUrlParser: true });
 
-const PORT = process.env.PORT || 3000;
-const db = require("./models");
+const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-require("./routes/html-routes.js")(app);
+app.use(require("./routes/html-routes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
