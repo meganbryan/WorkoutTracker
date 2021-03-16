027c756739dc74dbb8ad4250203207fe63b89690
Megan Bryan <meganmbryan12@gmail.com>
2021-03-16 10:55:21 -0700

    Update api routes to get past 7 workouts and not send json

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,31 +2,38 @@ const router = require("express").Router();
 const db = require("../models");
 
 router.get("/api/workouts", (req, res) => {
-    db.Workouts.find({})
+    db.Workouts.findOne({})
+    .sort({created_at: -1})
     .then(data => {
-        res.json(data)
-    }).catch(error => res.json(error));
-})
-
-router.get("/api/workouts", (req, res) => {
-    db.Workouts.findOne().sort({created_at: -1})
-    .then(data => {
-        res.json(data)
-    }).catch(error => res.json(error));
+        res.send(data)
+    })
+    .catch(error => res.send(error));
 })
 
 router.post("/api/workouts", (req, res) => {
     db.Workouts.create({}).then(data => {
-        res.json(data)
-    }).catch(error => res.json(error));
+        res.send(data)
+    })
+    .catch(error => res.send(error));
 });
 
 router.put("/api/workouts/:id", (req, res) => {
     const currentWorkout = req.params.id;
     db.Workouts.findByIdAndUpdate(currentWorkout, { $push: { exercises: req.body } })
     .then(data => {
-        res.json(data)
-    }).catch(error => res.json(error));
+        res.send(data)
+    })
+    .catch(error => res.send(error));
+})
+
+router.get("/api/workouts/range", (req,res)=>{
+    db.Workouts.find({})
+    .sort({_id: -1})
+    .limit(7)
+    .then(data =>{
+        res.send(data);
+    })
+    .catch(error => res.send(error));
 })
 
 module.exports = router;
